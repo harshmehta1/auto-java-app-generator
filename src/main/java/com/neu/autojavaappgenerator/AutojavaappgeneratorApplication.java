6810671521cc172e75cd4b7149e79fb54d6bfdfb
Harsh Mehta <harshsmehta1@gmail.com>
2019-10-01 14:05:25 -0400

    first commit

---
+++ src/main/java/com/neu/autojavaappgenerator/AutojavaappgeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.neu.autojavaappgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutojavaappgeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutojavaappgeneratorApplication.class, args);
+    }
+
+}
